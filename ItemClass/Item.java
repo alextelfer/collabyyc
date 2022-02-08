2e686eaeef2836a2f8386ec69726b958ad3c1e02
Corbin Wedge <90798282+CorbinWedge@users.noreply.github.com>
2022-02-08 08:10:14 -0700

    Update Item.java
    Added all getters and setters for Item class

--- ItemClass/Item.java
+++ ItemClass/Item.java
@@ -15,4 +15,54 @@ public class Item {
         //is.insert(itemID, vendorID, name, price, quantity, category);
     }
 
+    public int getItemID() {
+        return this.itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public int getVendorID() {
+        return this.vendorID;
+    }
+
+    public void setVendorID(int vendorID) {
+        this.vendorID = vendorID;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    
+
 }
