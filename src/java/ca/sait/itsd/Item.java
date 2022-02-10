6cebdb0ac374f21164411b3b1f646f27eb230c6f
Lovro <lovro.horvat@edu.sait.ca>
2022-02-10 10:09:17 -0700

    Vendor Class

--- src/java/ca/sait/itsd/Item.java
+++ src/java/ca/sait/itsd/Item.java
@@ -6,18 +6,11 @@ public class Item {
     double price;
     int quantity;
     String category;
-    
-    //a class that has the Connection Pool(can be renamed)
-    //ItemService is = new ItemService();
-    public void addItem(int itemID, int vendorID, String name, double price, int quantity, String category) {
-        //adds a new item to the database
-        //is.insert(itemID, vendorID, name, price, quantity, category);
-    }
 
-/*    public Vendor getVendorFinance() {
+    public Vendor getVendorFinance() {
         Vendor vendorInfo = new Vendor();
         return vendorInfo;
-    } */
+    } 
 
     public int getItemID() {
         return this.itemID;
