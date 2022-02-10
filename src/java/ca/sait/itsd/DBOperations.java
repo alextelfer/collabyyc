b40b9a727c59c3ae36457f5db33d6060cbf851bb
alextelfer <alextelfer96@gmail.com>
2022-02-09 22:03:31 -0700

    Update DBOperations.java

--- src/java/ca/sait/itsd/DBOperations.java
+++ src/java/ca/sait/itsd/DBOperations.java
@@ -5,10 +5,34 @@
  */
 package ca.sait.itsd;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Alex Telfer, Brady Belsher
  */
 public class DBOperations {
-    
+     public String getInventory() {
+        String result = "";
+        ConnectionPool pool = ConnectionPool.getInstance();
+        try {
+            Connection conn = pool.getConnection();
+            String sql = "select * from collabyyc.items;";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                result = result + rs.getInt(1) + "," + rs.getInt(2) + "," + rs.getString(3) +
+                    "," + rs.getDouble(4) + "," + rs.getInt(5) + "," + rs.getString(6) + ";";
+            }
+            rs.close();
+            ps.close();
+            pool.freeConnection(conn);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
 }
